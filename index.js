b5161d3b08a3417b5f860c0dcddbeb95aa1c358e
Mponeng <mantombiratego@gmail.com>
2017-05-22 15:57:05 +0200

    added route that displays users that has been greeted

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 var express = require('express');
 var app = express();
-var greetedNames = require('./name.js')
+// var greetedNames = require('./name.js');
+// var GreetedNames = greetedNames();
 var port = 3000;
+
+var list = [];
 
 app.get('/greetings/:name',function(req, res){
 var name = req.params.name
-res.send('Hello,' + name)
-})
+res.send('Hello,' + name);
+list.push(name);
+});
 
-app.get('/greeted',greetedNames.names{
-
-})
-
-app.get('/counter/:name',function(req, res){
-
-
-})
+app.get('/greeted', function(req, res) {
+    res.send(list);
+});
 
 app.listen(port,function(){
 
--- name.js
+++ name.js
@@ -3,14 +3,16 @@ module.exports = function(){
 var nameList = [];
 
 var names = function(req, res){
- res.send(nameList)
+res.send('Added names')
 };
 
 var add = function(req, res){
+// var list = req.query.list
+res.send('Added name');
+// nameList.push(list);
+ };
 
-var list = req.query.list
-
-};
-nameList.push(list);
-
+return {
+  names
+}
 };
