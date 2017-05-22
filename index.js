5b81e6265dc34b6dd42a25d0f139a63c92db9d8a
Mponeng <mantombiratego@gmail.com>
2017-05-22 12:53:43 +0200

    added new file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var greetedNames = require('./name.js')
 var port = 3000;
 
 app.get('/greetings/:name',function(req, res){
@@ -7,7 +8,14 @@ var name = req.params.name
 res.send('Hello,' + name)
 })
 
-app.get('/counter')
+app.get('/greeted',greetedNames.names{
+
+})
+
+app.get('/counter/:name',function(req, res){
+
+
+})
 
 app.listen(port,function(){
 
---
+++ name.js
@@ -0,0 +1,16 @@
+module.exports = function(){
+
+var nameList = [];
+
+var names = function(req, res){
+ res.send(nameList)
+};
+
+var add = function(req, res){
+
+var list = req.query.list
+
+};
+nameList.push(list);
+
+};
