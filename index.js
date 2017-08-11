59025edb703bbc881174e54090d8db6cc574f309
Mponeng <mantombiratego@gmail.com>
2017-08-11 11:40:39 +0200

    greeting app

--- index.js
+++ index.js
@@ -1,63 +1,94 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
-var bodyParser = require('body-parser')
-
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = 3000;
 
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-app.get('/', function (req, res) {
-    res.render('index');
-});
-
-
 var list = [];
-var uniqList =[]
-
-app.get('./greetings/:name',function(req, res){
-var name = req.params.name
-if(uniqList.indexOf(name) === -1){
-  uniqList.push(name)
-}
-res.send('Hello,' + name);
-  list.push(name);
-});
-
-app.get('./greeted', function(req, res) {
-    res.send(uniqList);
-});
-
-app.get('./counter/:name', function(req, res){
-var name = req.params.name;
-// console.log(name);
+var uniqList = [];
+var namesGreeted = {};
 var greetingCounter = 0;
-for(var i = 0; i<list.length; i++){
-  if(list[i].name = name){
-    greetingCounter++;
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+  // var namesGreeted = req.body.firstName;
+  // console.log(namesGreeted);
+  res.render('index')
+});
+
+
+app.post('/greetings', function(req, res) {
+  var string = ' ';
+  var name = req.body.firstName
+  var language = req.body.language
+
+  list.push(name);
+
+  if (language === 'English') {
+    // res.render('index', {
+    string = 'Hello' + " " + name
+    greetingCounter++
+    //   greetCount: "has been greeted " + greetingCounter++ + " " + 'times'
+    // })
+  } else if (language === 'IsiXhosa') {
+    // res.render('index', {
+    string = 'Molo' + " " + name
+    greetingCounter++
+    //
+    }
+   else if (language === 'Sotho') {
+    // res.render('index', {
+    string = 'Dumela' + " " + name
+    greetingCounter++
   }
-}
-res.send(name + " " + "has been greeted " + greetingCounter++ + " " +'times')
+
+  res.render('index', {
+    greeting : string,
+    greetCount: greetingCounter
+  })
+});
+//
+// app.get('./greeted', function(req, res) {
+//     res.send(uniqList);
+// });
+
+app.get('/counter', function(req, res){
+  var name = req.body.firstName
+  // if(uniqList.indexOf(name) === -1){
+  //   uniqList.push(name)
+  // }
+  //list.push(name)
+// var name = req.params.name;
+// // console.log(name);
+// if(namesGreeted[name] === undefined){
+//   greetingCounter++;
+//   namesGreeted[name] = name;
+// }
+// // for(var i = 0; i<list.length; i++){
+// //   if(list[i].name = name){
+// //     greetingCounter++;
+// //   }
+// // }
+res.send(list)
 })
 
-app.use((req, res, next) => {
-console.log('one')
-next();
-});
 
-app.use((req, res, next) => {
-console.log('two')
-next();
-});
 
-app.listen(port,function(){
+app.listen(port, function() {
 
-// var host = server.address().address;
-// var port = server.address().port;
+  // var host = server.address().address;
+  // var port = server.address().port;
 
-console.log('Example app listening at :' + port);
+  console.log('Example app listening at :' + port);
 
 });
