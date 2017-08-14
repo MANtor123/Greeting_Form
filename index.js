1f3aa42bc7d55484707e538ea8241754962a2900
Mponeng <mantombiratego@gmail.com>
2017-08-14 12:25:34 +0200

    added enviromental port

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ res.send(list)
 
 
 
-app.listen(port, function() {
+app.listen(process.env.PORT || 3000, function() {
 
   // var host = server.address().address;
   // var port = server.address().port;
